95a94bfa5f55fb351c116f2d456be48daa74dcbf
EHJ11 <usr3468@salesianos-lacuesta.net>
2026-05-14 10:14:52 +0100

    cambios

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,5 @@
 import { Drawer } from "expo-router/drawer";
-import "react-native-gesture-handler";
+// import "react-native-gesture-handler";
 export default function Layout() {
   return (
     <Drawer>
